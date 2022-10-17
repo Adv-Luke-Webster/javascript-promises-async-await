7e9dcfb92f8a1fe370cabfd3226920fe04d5946f
Adv-Luke-Webster <luke.webster@oneadvanced.com>
2022-10-17 14:01:27 +0100

    Module completed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
 import { fetchWithTimeout, fetchMovies, fetchBooks } from "./services";
 const movies = require("./data/movies.json");
 
-export function getBooksAndMovies(){
-    
+const getBooksAndMovies = () => {
+    return Promise.all([fetchBooks(), fetchMovies()])
+        .then(([books, movies]) => ({
+            books, movies
+        }))
+        .catch(error => console.log("Error fetchingbooks and movies", error))
 }
 
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then(results => {
+    console.log('getBooksAndMoviesPromise', results);
+});
+
+function getBooksOrMovies() {
+    return Promise.race([fetchBooks(), fetchMovies()])
+    .then(results => results)
+    .catch(error => {
+        console.log('Error waiting for the promice race', error);
+    });
+}
+
+const getBooksOrMoviesPromise = getBooksOrMovies();
+getBooksOrMoviesPromise.then(results => {
+    console.log('getBooksOrMoviesPromise', results);
+});
