c884bee33ddd277b35729543a62a7acd14410823
Adv-Luke-Webster <luke.webster@oneadvanced.com>
2022-10-17 13:45:44 +0100

    Module completed

--- src/services.js
+++ src/services.js
@@ -5,7 +5,7 @@ export function fetchWithTimeout(delay){
 };
 
 export function fetchMovies(){
-    return fetch("./data.movies.json")
+    return fetch("./data/movies.json")
         .then(response => response.json())
         .then(movies => movies)
         .catch(error => console.log(error));
