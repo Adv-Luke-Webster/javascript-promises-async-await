41b3e12a7680798a98aa16318923f19e3fd8f4d2
Adv-Luke-Webster <luke.webster@oneadvanced.com>
2022-10-17 13:44:01 +0100

    Module completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
-import { fetchWithTimeout } from "./services";
+import { fetchWithTimeout, fetchMovies, fetchBooks } from "./services";
 const movies = require("./data/movies.json");
 
-export function fetchMovies(){
-    const resolveFunction = () => movies;
-    return fetchWithTimeout(1000).then(resolveFunction);
+export function getBooksAndMovies(){
+    
 }
 
-const moviePromise = fetchMovies();
-moviePromise.then(results => {
-    console.log(results);
-});
