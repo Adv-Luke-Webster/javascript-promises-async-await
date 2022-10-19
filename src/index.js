4945d729ec4683cbeb0b9899ed3908b8779b895b
Adv-Luke-Webster <luke.webster@oneadvanced.com>
2022-10-19 09:51:20 +0100

    module completed

--- src/index.js
+++ src/index.js
@@ -46,23 +46,25 @@ async function getBooksOrMoviesAsync(){
         return values
     }
     catch(error){
-        console.log('Error waiting for the promise race', error)
+        console.error('Error waiting for the promise race', error)
     }
 }
 
-getBooksAndMoviesAsync().then(results)
-{
+getBooksAndMoviesAsync()
+.then(results => {
     console.log("movies and books", {
         movies: results.movies,
         books: results.books
     });
-}
+})
 
-getBooksOrMoviesAsync().then(results)
-{
+
+getBooksOrMoviesAsync()
+.then(results => {
     console.log("movies or books", {
         results
     });
-}
+})
+
 
 
